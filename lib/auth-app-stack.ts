007f03b1bebdb3a777238082012fd3beaf0219a6
Daniel-Wolski <20098120@mail.wit.ie>
2024-11-08 13:07:37 +0000

    Sign-in and signout resourcess

--- lib/auth-app-stack.ts
+++ lib/auth-app-stack.ts
@@ -56,6 +56,9 @@ export class AuthAppStack extends cdk.Stack {
       "confirm-signup.ts"
     );
 
+    this.addAuthRoute('signout', 'GET', 'SignoutFn', 'signout.ts');
+    this.addAuthRoute('signin', 'POST', 'SigninFn', 'signin.ts');
+
   }
 
   private addAuthRoute(
